dccf7e891830fa3d5bb98c270a08cd496f694441
shirabs <shira159159@gmail.com>
2019-11-28 23:30:07 +0200

    init

--- src/myMath/MonomTest2.java
+++ src/myMath/MonomTest2.java
@@ -1,77 +1,78 @@
-package Testing;
+package myMath;
 
-import static org.junit.jupiter.api.Assertions.*;
+	import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-import org.junit.rules.ErrorCollector;
+	import org.junit.jupiter.api.Test;
+	import org.junit.rules.ErrorCollector;
 
-import myMath.Monom;
+	import myMath.Monom;
 
-class MonomTest2 {
+	class MonomTest2 {
 
 
 
-	@Test
-	void testMonomDoubleInt() {
-		Monom x=new Monom(2,2);
-		assertEquals(2,x.get_coefficient());
-		assertEquals(2,x.get_power());
+		@Test
+		void testMonomDoubleInt() {
+			Monom x=new Monom(2,2);
+			assertEquals(2,x.get_coefficient());
+			assertEquals(2,x.get_power());
+		}
+
+		@Test
+		void testMonomMonom() {
+			Monom x=new Monom("9x^4");
+			Monom y=new Monom(x);
+			assertEquals(true,x.get_coefficient()==(y.get_coefficient()));
+			assertEquals(true,x.get_power()==(y.get_power()));
+		}
+
+		@Test
+		void testDerivative() {
+			Monom x=new Monom("2x^2");
+			//System.out.println(x.derivative());
+			assertEquals(4, x.derivative().get_coefficient());
+			assertEquals(1, x.derivative().get_power());
+		}
+
+		@Test
+		void testF() {
+			Monom y=new Monom("6x^2");
+			double x=3.5;
+			assertEquals(73.5, y.f(x));
+		}
+
+		@Test
+		void testIsZero() {
+			Monom a=new Monom(0, 0);
+			assertEquals(true, a.isZero());
+		}
+
+		@Test
+		void testMonomString() {
+			Monom x=new Monom("2x^2");
+			assertEquals(2,x.get_coefficient());
+			assertEquals(2,x.get_power());
+
+
+		}
+
+		@Test
+		void testAdd() {
+			Monom x=new Monom("2x^2");
+			Monom y=new Monom("6x^2");
+			x.add(y);
+			assertEquals(8, x.get_coefficient());
+			assertEquals(2, x.get_power());
+		}
+
+		@Test
+		void testMultipy() {
+			Monom x=new Monom("2x^2");
+			Monom y=new Monom("6x^2");
+			x.multipy(y);
+			assertEquals(12, x.get_coefficient());
+			assertEquals(4, x.get_power());
+		}
+
 	}
 
-	@Test
-	void testMonomMonom() {
-		Monom x=new Monom("9x^4");
-		Monom y=new Monom(x);
-		assertEquals(true,x.get_coefficient()==(y.get_coefficient()));
-		assertEquals(true,x.get_power()==(y.get_power()));
-	}
-
-	@Test
-	void testDerivative() {
-		Monom x=new Monom("2x^2");
-		//System.out.println(x.derivative());
-		assertEquals(4, x.derivative().get_coefficient());
-		assertEquals(1, x.derivative().get_power());
-	}
-
-	@Test
-	void testF() {
-		Monom y=new Monom("6x^2");
-		double x=3.5;
-		assertEquals(73.5, y.f(x));
-	}
-
-	@Test
-	void testIsZero() {
-		Monom a=new Monom(0, 0);
-		assertEquals(true, a.isZero());
-	}
-
-	@Test
-	void testMonomString() {
-		Monom x=new Monom("2x^2");
-		assertEquals(2,x.get_coefficient());
-		assertEquals(2,x.get_power());
-		
-		
-	}
-
-	@Test
-	void testAdd() {
-		Monom x=new Monom("2x^2");
-		Monom y=new Monom("6x^2");
-		x.add(y);
-		assertEquals(8, x.get_coefficient());
-		assertEquals(2, x.get_power());
-	}
-
-	@Test
-	void testMultipy() {
-		Monom x=new Monom("2x^2");
-		Monom y=new Monom("6x^2");
-		x.multipy(y);
-		assertEquals(12, x.get_coefficient());
-		assertEquals(4, x.get_power());
-	}
-
-}
